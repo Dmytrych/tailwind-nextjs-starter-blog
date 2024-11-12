77bb7ad10ddfb5e77fcf5e34a5bb4814e971fb39
Dmytro Khabaznia <d.khabaznia@KIE-X-L-0746.pt.playtech.corp>
2024-11-12 17:53:07 +0200

    Localization and google tag added

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -84,7 +84,7 @@ const ThemeSwitch = () => {
                         <div className="mr-2">
                           <Sun />
                         </div>
-                        Light
+                        Світла
                       </button>
                     )}
                   </Menu.Item>
@@ -100,7 +100,7 @@ const ThemeSwitch = () => {
                         <div className="mr-2">
                           <Moon />
                         </div>
-                        Dark
+                        Темна
                       </button>
                     )}
                   </Menu.Item>
@@ -116,7 +116,7 @@ const ThemeSwitch = () => {
                         <div className="mr-2">
                           <Monitor />
                         </div>
-                        System
+                        Авто
                       </button>
                     )}
                   </Menu.Item>
